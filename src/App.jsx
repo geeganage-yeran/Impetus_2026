f8e2f5258a81d863220cdc11e124052d9396fe7f
yeran lakvidu <geeganageyeran@gmail.com>
2025-10-16 20:39:01 +0530

    kl

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import HeroSection from './components/HeroSection'
 import NavBar from './components/NavBar'
@@ -15,8 +13,6 @@ import VenueSection from './components/VenueSection'
 import ContactSection from './components/ContactSection'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <NavBar/>
