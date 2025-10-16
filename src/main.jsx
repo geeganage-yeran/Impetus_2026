0a4c84e9053cd7e24142211bb6f90117a8a2b5ef
Yeran Lakvidu <147380668+geeganage-yeran@users.noreply.github.com>
2025-10-16 20:27:03 +0530

    Fix import path for AOS utility

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import initAOS from "./src/utils/aos";
+import initAOS from "./utils/aos.js";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
