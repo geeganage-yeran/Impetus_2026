5352a0f506fb1193525371f34526e3ceea1fdf6b
Yeran Lakvidu <147380668+geeganage-yeran@users.noreply.github.com>
2025-10-16 20:42:28 +0530

    Update AOS import path in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import initAOS from "./utils/aos";
+import initAOS from "./src/utils/aos";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
